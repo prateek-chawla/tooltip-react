12a858859542638b2d4200104b8bd715f3ba929f
Prateek Chawla <prateek.chawla98@gmail.com>
2020-08-09 23:54:41 +0530

    Updates

---
+++ precache-manifest.575f8c42eafaadbc684278b6e31a3066.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e5648905c1cc8ea0ba645d0af80100",
+    "url": "/tooltip-react/index.html"
+  },
+  {
+    "revision": "3c26ef72a690e3b6daba",
+    "url": "/tooltip-react/static/css/main.d1db7cfe.chunk.css"
+  },
+  {
+    "revision": "6004ca54cec74e5efff7",
+    "url": "/tooltip-react/static/js/2.90cbd8e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tooltip-react/static/js/2.90cbd8e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c26ef72a690e3b6daba",
+    "url": "/tooltip-react/static/js/main.098dfd56.chunk.js"
+  },
+  {
+    "revision": "cc4e1c19c22b3fd61136",
+    "url": "/tooltip-react/static/js/runtime-main.253b4ab2.js"
+  }
+]);
